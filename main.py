00e1a799c680cd8ee7046a7af6148ee021d3d64c
LadaLex0 <91010364+LadaLex0@users.noreply.github.com>
2021-10-11 18:22:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+cat={'name': (input('Write cat\’s name : ')),
+    'gender': (input('Write cat\’s gender')),
+    'age': (input('Write cat\’s age: ')),
+    'colour': (input('Write cat\’s colour: ')),
+    'breed': (input('Write cat\’s breed: ')),}
+print(cat)
